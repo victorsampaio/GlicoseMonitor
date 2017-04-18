cfe223e8a416647343acec1d0377cacf838451d7
Victor Sampaio <prolinsprojects@gmail.com>
2017-04-18 20:39:59 -0300

    Initial Project
    Initial Project

---
+++ GlicoseMonitor/app/src/main/java/vs/com/br/glicosemonitor/SplashScreenMainActivity.java
@@ -0,0 +1,13 @@
+package vs.com.br.glicosemonitor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreenMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen_main);
+    }
+}
