8ef99976054c945e876849b8fe3ca8936c4db3de
Victor Sampaio <prolinsprojects@gmail.com>
2017-04-18 23:42:39 -0300

    Developer branch
    Developer branch

--- GlicoseMonitor/app/src/main/java/vs/com/br/glicosemonitor/SplashScreenMainActivity.java
+++ GlicoseMonitor/app/src/main/java/vs/com/br/glicosemonitor/SplashScreenMainActivity.java
@@ -1,13 +1,38 @@
 package vs.com.br.glicosemonitor;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class SplashScreenMainActivity extends AppCompatActivity {
+import vs.com.br.glicosemonitor.view.MenuRegisterActivity;
 
+public class SplashScreenMainActivity extends AppCompatActivity implements Runnable{
+    private static final String Debug = " Debug: ";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen_main);
+
+        Handler action = new Handler();
+        int DELAY = 5000;
+        action.postDelayed(this, DELAY);
+
     }
+
+    public void run() {
+        Log.d(Debug, "Method - run() ");
+        //checkNetworkConnection();
+        startApplication();
+    }
+
+    private void startApplication() {
+        Log.d(Debug, "Method - startLogin() ");
+        Intent intent = new Intent(this, MenuRegisterActivity.class);
+        intent.putExtra("message", "Splash");
+        startActivity(intent);
+        finish();
+    }
+
 }
